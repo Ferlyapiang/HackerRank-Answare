6c3e4003243477f21b38677f8e9f71b260408c66
ferly.jeremi <ferly.jeremi@mncbank.co.id>
2023-06-23 15:34:35 +0700

    Hackerank Answer easy and medium

---
+++ Prepare/ArithmeticOperators.py
@@ -0,0 +1,51 @@
+# Task
+# The provided code stub reads two integers from STDIN,  a and b . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+# Example
+# a = 3
+# b=5
+
+# Print the following:
+
+# 8
+# -2
+# 15
+# Input Format
+
+# The first line contains the first integer, a.
+# The second line contains the second integer, b.
+
+# Constraints
+# 1 <= a <= 10 10
+# 1 <= b <= 10 10
+
+
+# Output Format
+
+# Print the three lines as explained above.
+
+# Sample Input 0
+
+# 3
+# 2
+# Sample Output 0
+
+# 5
+# 1
+# 6
+# Explanation 0
+# 3 + 2 = 5
+# 3 - 2 = 1
+# 3 * 2 = 6
+
+if __name__ == '__Prepare__':
+    a = 3 # Read the first integer
+    b = 4  # Read the second integer
+    
+    # Perform the calculations and print the results
+    print(a + b)  # Sum of the two numbers
+    print(a - b)  # Difference of the two numbers
+    print(a * b)  # Product of the two numbers
